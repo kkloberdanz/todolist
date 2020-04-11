38a1315e2696ce40e96ec075acb8bedc557ca0dc
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2020-04-11 13:39:03 -0500

    fixed to 4 space indent

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,51 @@
 <template>
-  <div id="app">
-    <Todos v-bind:todos="todos" />
-  </div>
+    <div id="app">
+        <Todos v-bind:todos="todos" />
+    </div>
 </template>
 
 <script>
 import Todos from './components/Todos';
 
 export default {
-  name: 'App',
-  components: {
-    Todos
-  },
+    name: 'App',
+    components: {
+        Todos
+    },
 
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: false
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        },
-      ]
+    data() {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    title: "Todo One",
+                    completed: false
+                },
+                {
+                    id: 2,
+                    title: "Todo Two",
+                    completed: false
+                },
+                {
+                    id: 3,
+                    title: "Todo Three",
+                    completed: false
+                },
+            ]
+        }
     }
-  }
 }
 </script>
 
 <style>
 * {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
 }
 
 body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
 }
 </style>
